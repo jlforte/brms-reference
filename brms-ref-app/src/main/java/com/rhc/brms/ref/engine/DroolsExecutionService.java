495126212960148e157ec42ac6acd9dbc9526ffe
Jeff Forte <jeff.l.forte@gmail.com>
2012-08-22 10:59:53 -0400

    adding changed util classes

--- brms-ref-app/src/main/java/com/rhc/brms/ref/engine/DroolsExecutionService.java
+++ brms-ref-app/src/main/java/com/rhc/brms/ref/engine/DroolsExecutionService.java
@@ -14,70 +14,48 @@ import org.apache.log4j.spi.LoggerFactory;
 import com.rhc.brms.ref.core.RulesApplicationInterface;
 import com.rhc.brms.ref.core.ExecutionResultsTransformer;
 import com.rhc.brms.ref.core.StatelessDroolsService;
-import com.rhc.brms.ref.util.AgendaGroupCommandUtil;
+import com.rhc.brms.ref.util.CommandBuilderUtil;
 
 /**
  * 
- * A simple service that will return a RulesServiceResponse object which reflects 
+ * A simple service that will return a RulesServiceResponse object which reflects
  * 
  */
 public class DroolsExecutionService implements RulesApplicationInterface<RulesServiceRequest, RulesServiceResponse> {
 
-   private static final Logger logger = Logger.getLogger(DroolsExecutionService.class);
+	private static final Logger logger = Logger.getLogger(DroolsExecutionService.class);
 
-   private StatelessDroolsService droolsService = new StatelessDroolsService();
-   private ExecutionResultsTransformer<RulesServiceResponse> transformer = new RulesServiceResultsTransformer();
+	private StatelessDroolsService droolsService = new StatelessDroolsService();
+	private ExecutionResultsTransformer<RulesServiceResponse> transformer = new RulesServiceResultsTransformer();
 
-   /*
-    * (non-Javadoc)
-    * @see com.cigna.drools.core.ApplicationInterface#executeAllRules(java.lang.Object)
-    */
-   public RulesServiceResponse executeAllRules(RulesServiceRequest request) {
-      
-      List<Command> commands = new ArrayList<Command>();
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.cigna.drools.core.ApplicationInterface#executeAllRules(java.lang.Object)
+	 */
+	public RulesServiceResponse executeAllRules(RulesServiceRequest request) {
 
-     
-      //commands.add( CommandFactory.newInsert( request.getRequestDate()));  // insert Request Date
-      
-      // insert Facts being checked
-//      MemberOccurrenceProfileAndIdentiferType[] memlist = request.getIndividualList().getIndividualArray(0).getMemberOccurrences().getMemberOccurrenceArray();
-//		for (MemberOccurrenceProfileAndIdentiferType mem : memlist){ 
-//			commands.add( CommandFactory.newInsert((MemberOccurrenceProfileAndIdentiferType)mem));
-//			System.out.println("Inserting Member");
-//			for (CoverageSummaryType cov : mem.getCoverageSummaries().getCoverageSummaryArray()){
-//				commands.add( CommandFactory.newInsert((CoverageSummaryType)cov));
-//				System.out.println("Inserting Coverage");
-//				for (BenefitOptionType benefit : cov.getBenefitOptions().getBenefitOptionArray()) {
-//					commands.add( CommandFactory.newInsert((BenefitOptionType)benefit));
-//					System.out.println("Inserting Benefit");
-//					for (CoverageOwnerSummaryType covOwner : benefit.getCoverageOwners().getCoverageOwnerArray()) {
-//						System.out.println("Inserting CoverageOwner");
-//						commands.add( CommandFactory.newInsert((CoverageOwnerSummaryType)covOwner));
-//					}
-//				}
-//			}
-//		}
+		List<Command> commands = new ArrayList<Command>();
 
-	  //Set the Response Object
-      RulesServiceResponse response = new RulesServiceResponse();
-      commands.add( CommandFactory.newInsert(response, "response")); 
-      //Set the Agenda groups
-      //commands.add( AgendaGroupCommandUtil.buildAgendaGroupSetFocusCommand( "FilterCoverages" ) );
-      //commands.add( AgendaGroupCommandUtil.buildAgendaGroupSetFocusCommand( "IdentifyCoverages" ) );
-      commands.add( CommandFactory.newFireAllRules() );
-      
-      //Execute commands
-      ExecutionResults results = droolsService.executeCommandList( commands );
-      response = transformer.transform(results);
-      response.setFiredRules( droolsService.getFiredActivations());
-   
-      return response;
-   }
-   
-   
-   /*
-    * This ought to go in a utility class
-    */
+		// Add list of commands to insert corresponding objects in request
+		commands.addAll(CommandBuilderUtil.buildInsertObjectCommands(request));
 
+		// Set the Response Object
+		RulesServiceResponse response = new RulesServiceResponse();
+		commands.add(CommandBuilderUtil.buildInsertResponseCommand(response));
+
+		// Set the Agenda groups
+		commands.addAll(CommandBuilderUtil.buildAgendaGroupFocusCommands());
+
+		// Build and add fireAllRules command for execution
+		commands.add(CommandBuilderUtil.buildFireAllRulesCommand());
+
+		// Execute commands
+		ExecutionResults results = droolsService.executeCommandList(commands);
+		response = transformer.transform(results);
+		response.setFiredRules(droolsService.getFiredActivations());
+
+		return response;
+	}
 
 }
---
+++ brms-ref-app/src/main/java/com/rhc/brms/ref/util/CommandBuilderUtil.java
@@ -0,0 +1,86 @@
+package com.rhc.brms.ref.util;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.drools.command.Command;
+import org.drools.command.CommandFactory;
+import org.drools.command.runtime.rule.AgendaGroupSetFocusCommand;
+import org.drools.command.runtime.rule.FireAllRulesCommand;
+import org.drools.command.runtime.rule.InsertObjectCommand;
+
+import com.rhc.brms.ref.engine.RulesServiceRequest;
+import com.rhc.brms.ref.engine.RulesServiceResponse;
+
+public class CommandBuilderUtil {
+
+	/**
+	 * Utility class to create an AgendaGroupSetFocusCommand for the incoming agendaGroup
+	 * parameter.
+	 * 
+	 * @param agendaGroup
+	 * @return agendaGroupCommand
+	 * 
+	 */
+	public static AgendaGroupSetFocusCommand buildAgendaGroupSetFocusCommand(String agendaGroup) {
+		AgendaGroupSetFocusCommand agendaGroupCommand = new AgendaGroupSetFocusCommand();
+		agendaGroupCommand.setName(agendaGroup);
+		return agendaGroupCommand;
+	}
+
+	public static InsertObjectCommand buildInsertResponseCommand(RulesServiceResponse response) {
+		InsertObjectCommand insertResponseCommand = new InsertObjectCommand();
+		insertResponseCommand.setObject(response);
+		insertResponseCommand.setOutIdentifier("response");
+		return insertResponseCommand;
+	}
+
+	public static FireAllRulesCommand buildFireAllRulesCommand() {
+		FireAllRulesCommand fireAll = new FireAllRulesCommand();
+		return fireAll;
+	}
+
+	public static Collection<Command> buildInsertObjectCommands(RulesServiceRequest request) {
+
+		// Create ArrayList of commands to return
+		ArrayList<Command> commands = new ArrayList<Command>();
+
+		// Create insert commands to add values from request
+
+//		 commands.add( CommandFactory.newInsert( request.getRequestDate()));
+//		
+//		 MemberOccurrenceProfileAndIdentiferType[] memlist =
+//		 request.getIndividualList().getIndividualArray(0).getMemberOccurrences().getMemberOccurrenceArray();
+//		 for (MemberOccurrenceProfileAndIdentiferType mem : memlist){
+//		 commands.add(
+//		 CommandFactory.newInsert((MemberOccurrenceProfileAndIdentiferType)mem));
+//		 System.out.println("Inserting Member");
+//		 for (CoverageSummaryType cov :
+//		 mem.getCoverageSummaries().getCoverageSummaryArray()){
+//		 commands.add( CommandFactory.newInsert((CoverageSummaryType)cov));
+//		 System.out.println("Inserting Coverage");
+//		 for (BenefitOptionType benefit :
+//		 cov.getBenefitOptions().getBenefitOptionArray()) {
+//		 commands.add( CommandFactory.newInsert((BenefitOptionType)benefit));
+//		 System.out.println("Inserting Benefit");
+//		 for (CoverageOwnerSummaryType covOwner :
+//		 benefit.getCoverageOwners().getCoverageOwnerArray()) {
+//		 System.out.println("Inserting CoverageOwner");
+//		 commands.add( CommandFactory.newInsert((CoverageOwnerSummaryType)covOwner));
+//		 }
+//		 }
+//		 }
+
+		return commands;
+	}
+
+	public static Collection<Command> buildAgendaGroupFocusCommands() {
+		ArrayList<Command> commands = new ArrayList<Command>();
+
+		commands.add(buildAgendaGroupSetFocusCommand("FilterCoverages"));
+		commands.add(buildAgendaGroupSetFocusCommand("IdentifyCoverages"));
+
+		return commands;
+	}
+
+}
