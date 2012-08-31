a67620f1a8cbb93fca75787e51ac344aca4f1683
Justin Holmes <jholmes@redhat.com>
2012-08-30 21:09:58 -0400

    Formatting

--- brms-ref-app/src/test/java/com/rhc/brms/ref/MortgageTest.java
+++ brms-ref-app/src/test/java/com/rhc/brms/ref/MortgageTest.java
@@ -20,30 +20,30 @@ public class MortgageTest {
 	private static HashMap<String, List<AfterActivationFiredEvent>> firedActivations;
 	private static MortageApplicationService droolsExecService = new MortageApplicationService();
 
-	private final static Long CUSTOMER_ID_1 = Long.valueOf(1);
-	private final static Long CUSTOMER_ID_2 = Long.valueOf(2);
-	private final static Long CUSTOMER_ID_3 = Long.valueOf(3);
+	private final static Long CUSTOMER_ID_1 = Long.valueOf( 1 );
+	private final static Long CUSTOMER_ID_2 = Long.valueOf( 2 );
+	private final static Long CUSTOMER_ID_3 = Long.valueOf( 3 );
 
-	private final static Long APPLICATION_ID_1 = Long.valueOf(1);
-	private final static Long APPLICATION_ID_2 = Long.valueOf(2);
-	private final static Long APPLICATION_ID_3 = Long.valueOf(3);
+	private final static Long APPLICATION_ID_1 = Long.valueOf( 1 );
+	private final static Long APPLICATION_ID_2 = Long.valueOf( 2 );
+	private final static Long APPLICATION_ID_3 = Long.valueOf( 3 );
 
 	@Test
 	public void shouldDoSomeStuff() {
-		MortageApplicationRequest request = new MortageApplicationRequest(createApplications(), createCustomers());
+		MortageApplicationRequest request = new MortageApplicationRequest( createApplications(), createCustomers() );
 
 		// Response the Rules engine
-		MortageApplicationResponse response = droolsExecService.executeAllRules(request);
-		
-		//Assert.assertNotNull( response );
+		MortageApplicationResponse response = droolsExecService.executeAllRules( request );
+
+		// Assert.assertNotNull( response );
 	}
 
 	private Set<Customer> createCustomers() {
 
 		Set<Customer> customers = new HashSet<Customer>();
-		customers.add(new Customer("Dave", 20, 700, CUSTOMER_ID_1));
-		customers.add(new Customer("Mike", 18, 750, CUSTOMER_ID_2));
-		customers.add(new Customer("Bryan", 28, 450, CUSTOMER_ID_3));
+		customers.add( new Customer( "Dave", 20, 700, CUSTOMER_ID_1 ) );
+		customers.add( new Customer( "Mike", 18, 750, CUSTOMER_ID_2 ) );
+		customers.add( new Customer( "Bryan", 28, 450, CUSTOMER_ID_3 ) );
 
 		return customers;
 	}
@@ -51,9 +51,9 @@ public class MortgageTest {
 	private Set<Application> createApplications() {
 
 		Set<Application> applications = new HashSet<Application>();
-		applications.add(new Application(new BigDecimal(15000), CUSTOMER_ID_1, APPLICATION_ID_1));
-		applications.add(new Application(new BigDecimal(75000), CUSTOMER_ID_3, APPLICATION_ID_2));
-		applications.add(new Application(new BigDecimal(85000), CUSTOMER_ID_2, APPLICATION_ID_3));
+		applications.add( new Application( new BigDecimal( 15000 ), CUSTOMER_ID_1, APPLICATION_ID_1 ) );
+		applications.add( new Application( new BigDecimal( 75000 ), CUSTOMER_ID_3, APPLICATION_ID_2 ) );
+		applications.add( new Application( new BigDecimal( 85000 ), CUSTOMER_ID_2, APPLICATION_ID_3 ) );
 
 		return applications;
 	}
