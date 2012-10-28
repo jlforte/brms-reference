706fdf0a4294d922e8b57a817589ad7a3119aac1
Justin Holmes <justinmichaelholmes@gmail.com>
2012-10-27 21:12:25 -0700

    testing file permision stuff

--- drools-reference-implementation/drools-sample-runtime/src/main/java/com/rhc/drools/reference/CommandListBuilder.java
+++ drools-reference-implementation/drools-sample-runtime/src/main/java/com/rhc/drools/reference/CommandListBuilder.java
@@ -5,10 +5,13 @@ import java.util.List;
 import org.drools.command.Command;
 
 /**
- * Drools Concern #2: Business Logic needed to interact with the Drools Runtime (i.e. insert domain objects, set agenda
- * groups and fire all rules). The Drools Batch Command API offers an excellent out of the box solution for this
- * problem. With the Batch Command API, our business logic is entirely separate from the Drools runtime framework which
- * makes for cleaner code.
+ * Drools Concern #2: Business Logic needed to insert objects into the Drools Runtime and manipulate the execution of
+ * the engine (e.g. setting agenda groups or firing rules). The Drools Batch Command API offers an excellent out of the
+ * box solution for this problem. With the Batch Command API, our business logic is entirely separate from the Drools
+ * runtime framework which makes for cleaner code. <br>
+ * <br>
+ * NOTE: We've left the task of retrieving objects from the Knowledge Session to the ExecutionResultsTransformer
+ * 
  * 
  * @param request
  * @return List of Drools Commands that encapsulate our business logic.
--- drools-reference-implementation/drools-sample-runtime/src/main/java/com/rhc/drools/reference/KnowledgeBaseBuilder.java
+++ drools-reference-implementation/drools-sample-runtime/src/main/java/com/rhc/drools/reference/KnowledgeBaseBuilder.java
@@ -4,18 +4,18 @@ import org.drools.KnowledgeBase;
 
 /**
  * Drools Concern #1: Accumulation of Knowledge Resources (i.e. rules, queries, workflows, templates, decision tables),
- * compilation of those resources into Knowledge Packages, and creation of a Knowledge Base from the Knowledge Packages.
- * This tends to be the element that varies most application to application, so we've left problem to be solved by a
- * concrete class. All this solution needs is a KnowledgeBase to returned - it doesn't care how you create it. Here are
- * just a few ways that you could tackle Knowledge Asset Management:
+ * compilation of those resources into Knowledge Packages, and the creation of a Knowledge Base from the Knowledge
+ * Packages. This tends to be the element that varies most application to application, so we've left problem to be
+ * solved by a concrete class. All this solution needs is a KnowledgeBase to returned - it doesn't care how you create
+ * it. Here are a few ideas:
  * 
- * 1) One time compilation from the class path using the KnowledgeBuilder (e.g MortgageApplicationKBaseBuilder TODO link
+ * 1) Runtime compilation from the class path using the KnowledgeBuilder (e.g MortgageApplicationKBaseBuilder TODO link
  * here) <br>
- * 2) Compilation of remote resources using a KnowledgeAgent. This is useful when rules live in Guvnor or a remote file
- * store. <br>
+ * 2) Compilation of remote resources using a KnowledgeAgent. This is useful when Knowledge Packages live in Guvnor or a
+ * remote file store. <br>
  * 3) Application wide caching strategy that builds and stores KnowledgeBases for numerous business processes. <br>
- * 4) Pre-compiled KnowledgePackages or KnowledgeBase. This can be done with the ant task or Guvnor. You can also
- * serialize these resources.
+ * 4) Pre-compiled or serialized KnowledgePackages or KnowledgeBase living on the classpath. You can use the Drools ant
+ * task, Guvnor or a custom solution here.
  */
 
 public interface KnowledgeBaseBuilder {
