a0716ca5a8db11d440459e95846d0254eb3fee01
sherl0cks <justinmichaelholmes@gmail.com>
2013-06-27 19:20:55 -0400

    changes to KnowledgeAgent to support scanning

--- drools-reference-implementation/drools-sample-runtime/src/main/java/com/rhc/drools/reference/KnowledgeAgentKnowledgeBaseBuilder.java
+++ drools-reference-implementation/drools-sample-runtime/src/main/java/com/rhc/drools/reference/KnowledgeAgentKnowledgeBaseBuilder.java
@@ -1,12 +1,14 @@
+package com.rhc.drools.reference;
+
 /* 
- * Written by Red Hat Consulting.	
+ * Written by Red Hat Consulting. 
  * 
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,12 +16,16 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.rhc.drools.reference;
+
+import java.io.IOException;
 
 import org.drools.KnowledgeBase;
 import org.drools.agent.KnowledgeAgent;
 import org.drools.agent.KnowledgeAgentFactory;
 import org.drools.io.Resource;
+import org.drools.io.ResourceChangeNotifier;
+import org.drools.io.ResourceChangeScanner;
+import org.drools.io.ResourceChangeScannerConfiguration;
 import org.drools.io.ResourceFactory;
 
 /**
@@ -31,13 +37,23 @@ import org.drools.io.ResourceFactory;
  */
 public class KnowledgeAgentKnowledgeBaseBuilder implements KnowledgeBaseBuilder {
 
+	// Should these be here or in the constructor?
 	private KnowledgeAgent kagent = KnowledgeAgentFactory.newKnowledgeAgent( "MyAgent" );
+	private ResourceChangeNotifier notifier = ResourceFactory.getResourceChangeNotifierService();
+	private ResourceChangeScanner scanner = ResourceFactory.getResourceChangeScannerService();
 
 	/**
 	 * Use the same syntax use in a change set: prefix with the protocol <code>http:</code> or <code>file:</code> or
 	 * <code>classpath:</code>
 	 */
 	private String path;
+
+	/**
+	 * In seconds
+	 */
+	private String scannerInterval;
+
+	private boolean isInit = false;
 
 	public KnowledgeAgentKnowledgeBaseBuilder( String path ) {
 		setPath( path );
@@ -46,30 +62,61 @@ public class KnowledgeAgentKnowledgeBaseBuilder implements KnowledgeBaseBuilder 
 	public KnowledgeAgentKnowledgeBaseBuilder() {
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.rhc.drools.reference.KnowledgeBaseBuilder#getKnowledgeBase()
-	 */
 	@Override
 	public KnowledgeBase getKnowledgeBase() {
+		if ( !isInit ) {
+			init();
+		}
+
 		// kagent handles caching for us
 		return this.kagent.getKnowledgeBase();
 	}
 
-	public Resource buildChangeSetResource( String path ) throws IllegalArgumentException {
+	private void init() {
+		if ( isInit ) {
+			this.scanner.stop();
+			this.notifier.stop();
+		}
+
+		Resource changeSet = buildChangeSetResource( path );
+		this.kagent.applyChangeSet( changeSet );
+
+		ResourceChangeScannerConfiguration sconf = scanner.newResourceChangeScannerConfiguration();
+		sconf.setProperty( "drools.resource.scanner.interval", scannerInterval );
+		scanner.configure( sconf );
+
+		scanner.start();
+		notifier.start();
+
+		isInit = true;
+	}
+
+	public Resource buildChangeSetResource( String path ) {
+		Resource resource;
+
 		if ( path.startsWith( "http:" ) ) {
 			// replace the prefix with nothing, thus leaving the file name
-			return ResourceFactory.newUrlResource( path.replaceAll( "http:", "" ) );
+			resource = ResourceFactory.newUrlResource( path.replaceAll( "http:", "" ) );
 		} else if ( path.startsWith( "file:" ) ) {
 			// replace the prefix with nothing, thus leaving the file name
-			return ResourceFactory.newFileResource( path.replaceAll( "file:", "" ) );
+			resource = ResourceFactory.newFileResource( path.replaceAll( "file:", "" ) );
 		} else if ( path.startsWith( "classpath:" ) ) {
 			// replace the prefix with nothing, thus leaving the file name
-			return ResourceFactory.newClassPathResource( path.replaceAll( "classpath:", "" ), getClass() );
+			resource = ResourceFactory.newClassPathResource( path.replaceAll( "classpath:", "" ), getClass() );
 		} else
 			throw new IllegalArgumentException(
 					"path is malformed: must be prefixed with http: classpath: or file: in order to indicate which resource to use" );
+
+		// This is a check to see if the resource exist - expect barf if it
+		// doesnt
+		try {
+			resource.getInputStream();
+		} catch ( IOException e ) {
+			System.err.println( "Java can't find your changeset" );
+			e.printStackTrace();
+		}
+
+		return resource;
 	}
 
 	public String getPath() {
@@ -78,8 +125,14 @@ public class KnowledgeAgentKnowledgeBaseBuilder implements KnowledgeBaseBuilder 
 
 	public void setPath( String path ) {
 		this.path = path;
-		Resource changeSet = buildChangeSetResource( path );
-		this.kagent.applyChangeSet( changeSet );
+	}
+
+	public void setScannerInternal( String interval ) {
+		this.scannerInterval = interval;
+	}
+
+	public String getScannerInterval() {
+		return this.scannerInterval;
 	}
 
 }
