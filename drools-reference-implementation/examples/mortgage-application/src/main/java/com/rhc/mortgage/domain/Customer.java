57508a415405a881f3f50613483330312f6ed72f
Justin Cohler <jcohler@localhost.localdomain>
2013-11-15 14:23:25 -0500

    Added equals() and hashCode() to Application and Customer classes

--- drools-reference-implementation/examples/mortgage-application/src/main/java/com/rhc/mortgage/domain/Customer.java
+++ drools-reference-implementation/examples/mortgage-application/src/main/java/com/rhc/mortgage/domain/Customer.java
@@ -62,4 +62,54 @@ public class Customer {
 				+ valid + "]";
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((age == null) ? 0 : age.hashCode());
+		result = prime * result
+				+ ((creditScore == null) ? 0 : creditScore.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((valid == null) ? 0 : valid.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Customer other = (Customer) obj;
+		if (age == null) {
+			if (other.age != null)
+				return false;
+		} else if (!age.equals(other.age))
+			return false;
+		if (creditScore == null) {
+			if (other.creditScore != null)
+				return false;
+		} else if (!creditScore.equals(other.creditScore))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (valid == null) {
+			if (other.valid != null)
+				return false;
+		} else if (!valid.equals(other.valid))
+			return false;
+		return true;
+	}
+
 }
